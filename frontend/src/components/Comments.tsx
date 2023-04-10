41c7a176b4b40a48339d4965ff501fe19f652b35
Jakub <gawlikjakub9@gmail.com>
2023-04-11 00:00:03 +0200

    creating comments component

---
+++ frontend/src/components/Comments.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import CommentsList from "./CommentsList";
+
+const Comments = () => {
+  return (
+    <div>
+      <div>
+        <CommentsList />
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Comments;
---
+++ frontend/src/components/CommentsList.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CommentsItem from "./CommentsItem";
+
+const CommentsList = () => {
+  return (
+    <div>
+      <CommentsItem />
+    </div>
+  );
+};
+
+export default CommentsList;
